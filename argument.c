47f4197ee7c20509a4f00135172a4502323a3394
rnitta <rnitta@student.42.fr>
2021-01-21 22:14:34 +0900

    modify arguments.c

--- argument.c
+++ argument.c
@@ -1,4 +1,4 @@
-//type   string:0  int:1
+#include <stdlib.h>
 
 #define ARG_SIZE 30
 #define ARG_TYPE_STR 0
@@ -15,7 +15,7 @@ typedef struct s_arg_list
 {
     t_arg   args[ARG_SIZE];
     int     arg_num;
-    s_arg_list  *next;
+    struct s_arg_list  *next;
 }               t_arg_list;
 
 int     arg_add(t_arg *arg, t_arg_list *arg_list)
@@ -27,18 +27,18 @@ int     arg_add(t_arg *arg, t_arg_list *arg_list)
         if (!arg_list->args[i].name)
             return (_bcopy_int(&arg_list[i], arg, sizeof(t_arg)));
     if (!arg_list->next)
-        if (!(arg_list->next = (t_arg_list *)malloc(sizof(t_arg_list))))
+        if (!(arg_list->next = (t_arg_list *)malloc(sizeof(t_arg_list))))
             return (-1);
         else
             _bzero(arg_list->next, sizeof(t_arg_list));
-    return (arg_add(arg, arg_list->next));)
+    return (arg_add(arg, arg_list->next));
 }
 
 int     arg_free(t_arg *arg)
 {
     free(arg->name);
     if (arg->type == ARG_TYPE_STR)
-        free(*(arg->data));
+        free(*(char *)(arg->data));
     free(arg->data);
     arg->name = NULL;
     arg->data = NULL;
