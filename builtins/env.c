565af8f1756c1d4796c23d61bc694384899b0e5a
syudai <yudai14142@gmail.com>
2021-02-04 18:05:01 +0900

    modify echo

--- builtins/env.c
+++ builtins/env.c
@@ -3,9 +3,18 @@
 int ft_env(t_arg_main *arg_main)
 {
     int i;
-// arg_list_getでenvs作成
+    int j;
+    char **envs;
+
     i = 0;
+    envs = arg_list_get(arg_main, 0);
     while (env[i])
+    {
+        j = 0;
+        while (env[i][j])
+            if (env[i][j++] == -1)
+                break;
         printf("%s\n", env[i++]);
+    }
     return (0);
 }
