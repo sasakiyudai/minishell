9030b57cbe56d84a9d79f512792fec5805d601a8
rnittaBadCommit <ryushironitta@yahoo.co.jp>
2021-02-06 23:38:20 +0900

    norm argument.c

--- argument.c
+++ argument.c
@@ -256,20 +256,20 @@ void	arg_delete(t_arg_main *arg_main, char *name)
 			arg_main->head.next = tmp;
 }
 
-t_arg_list	*_arg_isexist_process(t_arg_list *arg_list, char *name)
+t_arg_list	*arg_isexist_process(t_arg_list *arg_list, char *name)
 {
 	if (!arg_list->next)
 		return (NULL);
 	if (!(ft_strcmp(arg_list->next->arg.name, name)))
 		return (arg_list->next);
-	return (_arg_isexist_process(arg_list->next, name));
+	return (arg_isexist_process(arg_list->next, name));
 }
 
-t_arg_list   *arg_isexist(t_arg_main *arg_main, char *name)
+t_arg_list	*arg_isexist(t_arg_main *arg_main, char *name)
 {
 	if (!ft_strcmp(name, "?"))
 		return (&(arg_main->head));
-	return (_arg_isexist_process(&(arg_main->head), name));
+	return (arg_isexist_process(&(arg_main->head), name));
 }
 
 int		arg_get(t_arg_main *arg_main, t_arg *arg, char *name)
