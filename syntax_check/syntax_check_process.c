6e626020c680835446a3acac3f6ca3094ee82976
rnittaBadCommit <ryushironitta@yahoo.co.jp>
2021-02-10 19:54:07 +0900

    syntax

--- syntax_check/syntax_check_process.c
+++ syntax_check/syntax_check_process.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 18:14:10 by marvin            #+#    #+#             */
-/*   Updated: 2021/02/10 19:52:22 by marvin           ###   ########.fr       */
+/*   Updated: 2021/02/10 19:53:58 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		syntax_check_main(char *cmd)
 	t_syntax_flag	syntax_flag;
 
 	ft_bzero(&syntax_flag, sizeof(t_syntax_flag));
-	syntax_flag.pipe = 1;
+	syntax_flag.pipe = 2;
 	syntax_flag.semi = 1;
 	while (*cmd)
 	{
