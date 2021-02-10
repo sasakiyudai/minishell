569a970e8c9b7a01e425000439df5c5114dd4cec
rnittaBadCommit <ryushironitta@yahoo.co.jp>
2021-02-10 13:51:50 +0900

    minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -25,31 +25,31 @@ typedef struct s_arg
 	char *name;
 	int type;
 	void *data;
-}               t_arg;
+}				t_arg;
 
 typedef struct s_arg_list
 {
-	t_arg   arg;
-	struct s_arg_list  *next;
-}               t_arg_list;
+	t_arg	arg;
+	struct s_arg_list	*next;
+}				t_arg_list;
 
 typedef struct s_arg_main
 {
-	t_arg_list  head;
+	t_arg_list	head;
 	char ***raw;
 	int		pwd_slash;
 	int			arg_num;
 	int			std_in;
 	int			std_out;
-}               t_arg_main;
+}				t_arg_main;
 
-typedef    struct    s_split
+typedef	struct	s_split
 {
-    int        cnt_splitnum;
-    int        cnt_moji;
-    int        flag_sequencial;
-    char    bitflag_quote;
-}                t_split;
+	int		cnt_splitnum;
+	int		cnt_moji;
+	int		flag_sequencial;
+	char	bitflag_quote;
+}				t_split;
 
 typedef struct	s_syntax_flag
 {
@@ -74,11 +74,11 @@ void			just_for_norm(char ***raw_cmd);
 void			bin_e(char ***raw_cmd);
 void			one_command_bin_e(char ***raw_cmd, t_arg_main *arg_main);
 int				rare_exception(char ***raw_cmd);
-int             is_ok(char *command, int i);
-int             is_space(char c);
-int             is_redirect(char c);
-int             num_of_redirect(char **tab);
-int             ft_tablen(char **tab);
+int				is_ok(char *command, int i);
+int				is_space(char c);
+int				is_redirect(char c);
+int				num_of_redirect(char **tab);
+int				ft_tablen(char **tab);
 char			*arg_to_str_quotes(t_arg *arg);
 void			remove_quotes(char *cmd);
 void			set_hatena(t_arg_main *arg_main, int i);
@@ -95,7 +95,7 @@ int 			ft_exit(char *args[]);
 int				ft_export(char *args[], t_arg_main *arg_main);
 int				ft_unset(char *argv[], t_arg_main *arg_main);
 int				ft_env(t_arg_main *arg_main);
-void    		arg_free(t_arg *arg);
+void			arg_free(t_arg *arg);
 int				arg_copy(t_arg *dest, t_arg *src);
 int 			arg_new(t_arg_main *arg_main, t_arg *src);
 char			***make_command_array(char *cmd);
@@ -120,32 +120,32 @@ int				arg_get(t_arg_main *arg_main, t_arg *arg, char *name);
 int				arg_list_ini(t_arg_main *arg_main);
 void			arg_list_ini_process(t_arg_list *arg_list);
 int				get_path(t_arg_main *arg_main, char **ret, char *name);
-char    		*ft_strndup(char *src, int n);
-int     		cnt_splitnum_command(char *s, char c);
-char    		**split_command_free(char **ret, int cnt_splitnum);
-void    		free_split_command_all(char **s);
-void    		check_quote(char c, char *bitflag_quote);
-void    		split_make_str(char **s, t_split *split_arg, char **ret);
+char			*ft_strndup(char *src, int n);
+int	 			cnt_splitnum_command(char *s, char c);
+char			**split_command_free(char **ret, int cnt_splitnum);
+void			free_split_command_all(char **s);
+void			check_quote(char c, char *bitflag_quote);
+void			split_make_str(char **s, t_split *split_arg, char **ret);
 int				split_command_ini(char **s, char c, t_split *split_arg, char ***ret);
-void    		split_command_last(char *s, t_split *split_arg, char ***ret);
-char    		**split_command(char *s, char c);
+void			split_command_last(char *s, t_split *split_arg, char ***ret);
+char			**split_command(char *s, char c);
 void			split_free_all(char **s);
-void    		arg_free(t_arg *arg);
+void			arg_free(t_arg *arg);
 char			*surround_minus_one(char *value);
 char			*ft_strjoin_free(char *s1, char *s2);
-int    			arg_copy(t_arg *dest, t_arg *src);
-int     		arg_new(t_arg_main *arg_main, t_arg *src);
+int				arg_copy(t_arg *dest, t_arg *src);
+int	 			arg_new(t_arg_main *arg_main, t_arg *src);
 int 			arg_add(t_arg_main *arg_main, t_arg *arg);
-int    			arg_main_ini(t_arg_main *arg_main);
+int				arg_main_ini(t_arg_main *arg_main);
 int 			arg_charlen(t_arg *arg, int quote);
 int 			ft_strcpy_int(char *dest, char *src);
 char 			*arg_to_str(t_arg *arg);
-void   			arg_delete(t_arg_main *arg_main, char *name);
-t_arg_list   	*_arg_isexist_process(t_arg_list *arg_list, char *name);
-t_arg_list   	*arg_isexist(t_arg_main *arg_main, char *name);
-int     		arg_get(t_arg_main *arg_main, t_arg *arg, char *name);
-int    			arg_list_ini(t_arg_main *arg_main);
-void    		arg_list_ini_process(t_arg_list *arg_list);
+void			arg_delete(t_arg_main *arg_main, char *name);
+t_arg_list		*_arg_isexist_process(t_arg_list *arg_list, char *name);
+t_arg_list		*arg_isexist(t_arg_main *arg_main, char *name);
+int	 			arg_get(t_arg_main *arg_main, t_arg *arg, char *name);
+int				arg_list_ini(t_arg_main *arg_main);
+void			arg_list_ini_process(t_arg_list *arg_list);
 int				is_dollarble(char c);
 char			fff(char flag);
 int 			ft_len(char *args[]);
